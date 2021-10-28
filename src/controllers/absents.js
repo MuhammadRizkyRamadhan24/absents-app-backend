c33baf680e79440278c82aa38b08339a07d6c356
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-10-28 14:11:32 +0700

    update

--- src/controllers/absents.js
+++ src/controllers/absents.js
@@ -281,37 +281,65 @@ exports.inputAbsent = (req, res) => {
         : `${date.getMonth() + 1}`;
     let autoDate =
       date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
-    let absen1 = `${date.getFullYear()}-${autoMonth}-${autoDate} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-    let masuk = `${date.getFullYear()}-${autoMonth}-${autoDate}`;
-    let absen = new Date(`${absen1}`);
-    let jadwal = new Date(`${masuk} 08:00:00`);
+
+    let masuk1 = `${date.getFullYear()}-${autoMonth}-${autoDate} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    console.log(masuk1);
+    let masuk2 = `${date.getFullYear()}-${autoMonth}-${autoDate}`
+    let absen = new Date(`${masuk1}`);
+    let jadwal = new Date(`${masuk2} 08:00:00`);
     let diffMs = jadwal - absen;
+    let diffDays = Math.floor(diffMs / 86400000);
     let diffHrs = Math.floor((diffMs % 86400000) / 3600000);
-    console.log(diffHrs);
     let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
     if (diffHrs < 0) {
-      const late = `Terlambat ${Math.abs(diffHrs)} jam, ${Math.abs(
-        diffMins
-      )} menit`;
-      console.log(id, type, late);
-      inputAbsent(id, type, late, (err, results) => {
-        if (!err) {
-          return standardResponse(res, 200, true, "Success Input Absent");
-        } else {
-          return standardResponse(res, 400, false, "An error occured");
-        }
-      });
+      console.log(
+        "Terlambat " +
+          Math.abs(diffHrs) +
+          " Jam, " +
+          Math.abs(diffMins) +
+          " Menit"
+      );
     } else if (diffHrs >= 0) {
-      const late = "Tepat waktu";
-      console.log(id, type, late);
-      inputAbsent(id, type, late, (err, results) => {
-        if (!err) {
-          return standardResponse(res, 200, true, "Success Input Absent");
-        } else {
-          return standardResponse(res, 400, false, "An error occured");
-        }
-      });
+      console.log("Tepat Waktu");
     }
+    // let date = new Date();
+    // let autoMonth =
+    //   date.getMonth() + 1 < 10
+    //     ? `0${date.getMonth() + 1}`
+    //     : `${date.getMonth() + 1}`;
+    // let autoDate =
+    //   date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
+    // let absen1 = `${date.getFullYear()}-${autoMonth}-${autoDate} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    // let masuk = `${date.getFullYear()}-${autoMonth}-${autoDate}`;
+    // let absen = new Date(`${absen1}`);
+    // let jadwal = new Date(`${masuk} 08:00:00`);
+    // let diffMs = jadwal - absen;
+    // let diffHrs = Math.floor((diffMs % 86400000) / 3600000);
+    // console.log(diffHrs);
+    // let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
+    // if (diffHrs < 0) {
+    //   const late = `Terlambat ${Math.abs(diffHrs)} jam, ${Math.abs(
+    //     diffMins
+    //   )} menit`;
+    //   console.log(id, type, late);
+    //   inputAbsent(id, type, late, (err, results) => {
+    //     if (!err) {
+    //       return standardResponse(res, 200, true, "Success Input Absent");
+    //     } else {
+    //       return standardResponse(res, 400, false, "An error occured");
+    //     }
+    //   });
+    // } else if (diffHrs >= 0) {
+    //   const late = "Tepat waktu";
+    //   console.log(id, type, late);
+    //   inputAbsent(id, type, late, (err, results) => {
+    //     if (!err) {
+    //       return standardResponse(res, 200, true, "Success Input Absent");
+    //     } else {
+    //       return standardResponse(res, 400, false, "An error occured");
+    //     }
+    //   });
+    // }
   } else if (type === "pulang") {
     const late = "-";
     inputAbsent(id, type, late, (err, results) => {
